c36a6ea53f026d2c6e1de830db31c3e699dd7a9c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-03 04:22:21 -0500

    Add more input fields

--- CardGenerator/react-canvas-draw/src/Components/EditPanel.js
+++ CardGenerator/react-canvas-draw/src/Components/EditPanel.js
@@ -10,7 +10,43 @@ function EditPanel({ card , setCard, updateCard}) {//
                 card.name = e.target.value;
                 updateCard(card);
             }}
-            value={card.name}></input>
+                value={card.name}></input>
+
+            {/* Species */}
+            <input type="text" onChange={(e) => {
+                card.species = e.target.value;
+                updateCard(card);
+            }}
+                value={card.species}></input>
+
+            {/* Species */}
+            <input type="text" onChange={(e) => {
+                let tags = "" + e.target.value;
+                card.tags = tags.split(", ");
+                updateCard(card);
+            }}
+                value={card.tags.join(", ")}></input>
+
+            {/* Base Power */}
+            <input type="text" onChange={(e) => {
+                card.basePower = e.target.value*1;
+                updateCard(card);
+            }}
+                value={card.basePower}></input>
+
+            {/* Ability TEST */}
+            <input type="text" onChange={(e) => {
+                card.ability = e.target.value;
+                updateCard(card);
+            }}
+                value={card.ability}></input>
+
+            {/* Flavor Text */}
+            <input type="text" onChange={(e) => {
+                card.flavorText = e.target.value;
+                updateCard(card);
+            }}
+            value={card.flavorText}></input>
         </div>
     );
 }
