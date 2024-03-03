b3942940b21af21d699c6831acd4d80f855b8a68
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-02 21:22:56 -0500

    Basic card layout

--- CardGenerator/react-canvas-draw/src/Components/Canvas.js
+++ CardGenerator/react-canvas-draw/src/Components/Canvas.js
@@ -19,11 +19,22 @@ function Canvas() {
     let card = makeCardTest();
 
     const bufferBase = 0.1 * RESOLUTION;
+    const textRow = 0.25 * RESOLUTION;
+    const textAreaY = 2.4 * RESOLUTION;
+    const textAreaH = 1 * RESOLUTION;
     let boxes = [
       // border
       { c: 'black', x: 0, y: 0, w: width, h: height },
       // base
       { c: 'white', x: 0 + bufferBase, y: 0 + bufferBase, w: width - bufferBase * 2, h: height - bufferBase * 2 },
+      //type
+      { c: '#dbd69e', x: 0 + bufferBase, y: textRow * 2 + bufferBase, w: width - bufferBase * 2, h: textRow },
+      //text border
+      { c: 'black', x: 0, y: textAreaY, w: width, h: textAreaH },
+      //text area
+      { c: '#dbd69e', x: 0 + bufferBase, y: textAreaY + bufferBase / 2, w: width - bufferBase * 2, h: textAreaH - bufferBase },
+      //card info
+      { c: 'black', x: 0, y: height - bufferBase * 2, w: width, h: bufferBase * 2 },
     ];
     boxes.forEach(box => {
       context.fillStyle = box.c;
@@ -48,7 +59,7 @@ function Canvas() {
   return (
     <div>
       <canvas ref={canvasRef}
-        style={{ width: "250px", height: "350px", display: "block" }}
+        style={{ width: "500px", height: "700px", display: "block" }}
       />
       <button onClick={saveImage}>Save Image</button>
     </div>
