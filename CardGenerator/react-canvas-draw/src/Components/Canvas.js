66322f655f4f50aa4dc86042575c7189bf832b31
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-02 20:27:13 -0500

    Setup canvas

--- CardGenerator/react-canvas-draw/src/Components/Canvas.js
+++ CardGenerator/react-canvas-draw/src/Components/Canvas.js
@@ -3,14 +3,22 @@
 //2024-03-02: copied from https://www.dhiwise.com/post/designing-stunning-artwork-with-react-canvas-draw
 import React, { useRef, useEffect } from 'react';
 
+const RESOLUTION = 300;
+const width = 2.5 * RESOLUTION;
+const height = 3.5 * RESOLUTION;
+
 function Canvas() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
+    canvas.width = width;
+    canvas.height = height;
     const context = canvas.getContext('2d');
 
     // Start drawing
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
     context.beginPath();
     context.arc(100, 100, 50, 0, 2 * Math.PI);
     context.fillStyle = 'red';
@@ -29,7 +37,9 @@ function Canvas() {
 
   return (
     <div>
-      <canvas ref={canvasRef} />
+      <canvas ref={canvasRef}
+        style={{ width: "250px", height: "350px", display: "block" }}
+      />
       <button onClick={saveImage}>Save Image</button>
     </div>
   );
