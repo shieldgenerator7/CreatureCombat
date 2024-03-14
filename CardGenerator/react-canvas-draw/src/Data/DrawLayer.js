4c926d0232d6bd10c22a6aa8025f024a6d49d231
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-14 10:59:53 -0400

    Add class: DrawLayer

---
+++ CardGenerator/react-canvas-draw/src/Data/DrawLayer.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import Vector2, { VECTOR2_ZERO } from "./Vector2";
+
+export const DRAWLAYER_BOX = "box";
+export const DRAWLAYER_IMAGE = "image";
+
+class DrawLayer {
+    constructor(type, color = 'black', pos = VECTOR2_ZERO, size = VECTOR2_ZERO, infoFunc) {
+        this.type = type;
+        this.color = color;
+        this.position = pos.clone();
+        this.size = size.clone();
+        this.infoFunc = infoFunc;
+    }
+
+    getInfo(card) {
+        return this.infoFunc(card);
+    }
+}
+
+export default DrawLayer;
