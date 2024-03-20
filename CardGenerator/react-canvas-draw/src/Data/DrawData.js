6e9cc63cbb9508520188022a0e5f1d3b817d78eb
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-20 03:51:52 -0400

    Normalize spacing in draw data

--- CardGenerator/react-canvas-draw/src/Data/DrawData.js
+++ CardGenerator/react-canvas-draw/src/Data/DrawData.js
@@ -6,6 +6,12 @@ import Vector2, { VECTOR2_ZERO } from "./Vector2";
 export function generateCardSkin(width, height, margin, padding) {
     const rowheight = height / 17;
     const marginWidth = width - margin * 2;
+    const markersY = [
+        margin + rowheight * 2.4,
+        margin + rowheight * 3.4,
+        margin + rowheight * 11.4,
+        margin + rowheight * 15.4,
+    ];
     let cardSkin = [
         //black
         new DrawLayer(
@@ -26,39 +32,39 @@ export function generateCardSkin(width, height, margin, padding) {
         new DrawLayer(
             DRAWLAYER_IMAGE,
             undefined,
-            new Vector2(margin, margin + rowheight * 5),
-            new Vector2(marginWidth, rowheight * 8),
+            new Vector2(margin, markersY[1]),
+            new Vector2(marginWidth, markersY[2]-markersY[1]),
             (card) => card.imgPortrait
         ),
         //type bg
         new DrawLayer(
             DRAWLAYER_BOX,
             '#dbd69e',
-            new Vector2(margin, margin + rowheight * 4),
-            new Vector2(marginWidth, rowheight * 1),
+            new Vector2(margin, markersY[0]),
+            new Vector2(marginWidth, markersY[1]-markersY[0]),
             (card) => card.colors[1]
         ),
         //text area bg
         new DrawLayer(
             DRAWLAYER_BOX,
             '#dbd69e',
-            new Vector2(margin, margin + rowheight * 13),
-            new Vector2(marginWidth, rowheight * 4),
+            new Vector2(margin, markersY[2]),
+            new Vector2(marginWidth, markersY[3]-markersY[2]),
             (card) => card.colors[1]
         ),
         //text border
         new DrawLayer(
             DRAWLAYER_BOX,
             'black',
-            new Vector2(margin, margin + rowheight * 13),
+            new Vector2(margin, markersY[2]),
             new Vector2(marginWidth, rowheight * 0.2)
         ),
         //card info bg
         new DrawLayer(
             DRAWLAYER_BOX,
             'black',
-            new Vector2(margin, height - margin - padding),
-            new Vector2(marginWidth, rowheight * 1)
+            new Vector2(margin, markersY[3]),
+            new Vector2(marginWidth, rowheight * 2)
         ),
     ];
     return cardSkin;
