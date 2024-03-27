731c466cc5d3d30e4aacb270cc7d46d7f0911195
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-03-27 01:22:48 -0400

    Modify Counter

--- CardGenerator/react-canvas-draw/src/Components/Counter.js
+++ CardGenerator/react-canvas-draw/src/Components/Counter.js
@@ -1,16 +1,33 @@
 "use strict";
 
 function Counter({ value, setValue, allowNegative = false }) {
+    //
+    const set = (v) => {
+        let newValue = v;
+        if (!allowNegative) {
+            newValue = Math.max(0, newValue);
+        }
+        setValue(newValue);
+    };
     return (
         <div>
-            <input type="number" className="field" onChange={(e) => {
-                let newValue = e.target.value * 1;
-                if (!allowNegative) {
-                    newValue = Math.max(0, newValue);
-                }
-                setValue(newValue);
+            <input type="text" inputMode="numeric" className="counter" onChange={(e) => {
+                let v = e.target.value * 1;
+                set(v);
             }}
-                value={value.toString()}></input>
+                value={`${value}`}></input>
+            <button
+                onClick={(e) => {
+                    set(value + 1);
+                }}
+                onContextMenu={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    set(value - 1);
+                }}
+            >
+                + -
+            </button>
         </div>
     );
 }
