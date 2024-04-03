660812bb654ecfc45cf793167ec50900a1d31df9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-04-02 21:46:12 -0400

    Update Ability.js

--- CardGenerator/react-canvas-draw/src/Data/Ability/Ability.js
+++ CardGenerator/react-canvas-draw/src/Data/Ability/Ability.js
@@ -3,25 +3,44 @@
 import { abilityAtoms } from "../AbilityData";
 import AbilityLine from "../AbilityLine";
 
+const stringifyAbility = [
+    "name",
+    "codeText",
+    "params",
+]
+
 class Ability {
     constructor(name, codeText, params) {
         this.name = name;
         this.codeText = codeText;
-        this.lines = this.codeText.split("\n").slice(1).map(line => new AbilityLine);
+        this.lines = this.codeText
+            .split("\n")
+            .slice(1)
+            .map(line => new AbilityLine(line));
         this.params = params;
     }
 
     get FullText() {
         let sentenceStart = true;
-        let text = this.lines.forEach((line, i, arr) => {
+        let text = this.lines.map((line, i, arr) => {
             let atom = abilityAtoms[line.atom];
-            let segment = atom.text;
+            let segment = atom.text.trim();
             //Object.getOwnPropertyNames().forEach();
             atom.params.forEach(([key, value], j) => {
                 segment.replace(`{${key}}`, params[i][j]);
-                //...
             })
-            //...
-        });
+            if (sentenceStart) {
+                segment = segment.substring(0, 1).toUpperCase() + segment.substring(1);
+                sentenceStart = false;
+            }
+            let sentenceEnd = true;//TODO: make this check current and next line
+            if (sentenceEnd) {
+                segment += ".";
+                sentenceStart = true;
+            }
+            return segment;
+        })
+            .join(" ");
     }
 }
+export default Ability;
