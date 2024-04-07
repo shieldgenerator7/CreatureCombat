653c892b13a374c28d5086673c473a995034574a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-04-07 04:36:50 -0400

    #1 remove old cost methods from creature and ability

--- CardGenerator/react-canvas-draw/src/Data/Ability.js
+++ CardGenerator/react-canvas-draw/src/Data/Ability.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import { abilityCosts, abilityEffects, abilityRequirements } from "./AbilityData";
-
-const ABILITY_POINTCOST_FACTOR = 3;
-
 class Ability {
     constructor() {
         this.name = "";
@@ -19,109 +15,6 @@ class Ability {
         this.costReqText = "";//what it costs to play this ability (for custom abilities)
         this.effectText = "";//what this ability does (for custom abilities)
         this.effectCost = 10;//how much it costs to include this effect (for custom abilities)
-    }
-
-
-    get abilityCost() {
-        return abilityCosts
-            .find(fx => fx.name == this.costName);
-    }
-    get abilityRequirement() {
-        return abilityRequirements
-            .find(fx => fx.name == this.requirementName);
-    }
-    get abilityEffect() {
-        return abilityEffects
-            .find(fx => fx.name == this.effectName);
-    }
-
-    get CostText() {
-        return this.abilityCost
-            ?.getText(this.costX)
-            ?? "";
-    }
-    get RequirementText() {
-        if (this.costReqText && ["none", "custom"].includes(this.requirementName)) {
-            return this.costReqText;
-        }
-        return this.abilityRequirement
-            ?.getText(this.requirementX)
-            ?? "";
-    }
-    get EffectText() {
-        if (this.effectText && this.effectName == "custom") {
-            return this.effectText.trim();
-        }
-        return this.abilityEffect
-            ?.getText(this.effectX)
-            ?? "";
-    }
-
-    get CostSymbol() {
-        return this.abilityCost
-            ?.getSymbol(this.costX)
-            ?? "";
-    }
-    get RequirementSymbol() {
-        if (this.costReqText && ["none", "custom"].includes(this.requirementName)) {
-            return this.costReqText;
-        }
-        return this.abilityRequirement
-            ?.getSymbol(this.requirementX)
-            ?? "";
-    }
-
-    get FullText() {
-        let reqsym = this.RequirementSymbol;
-        let reqstr = (reqsym) ? ` (${reqsym})` : "";
-        return `*${this.name}*   ${this.CostSymbol}${reqstr}: ${this.EffectText}`;
-    }
-
-    get FullTextWithReminders() {
-        let fulltext = this.FullText;
-        let reqsym = this.RequirementSymbol;
-        if (reqsym) {
-            fulltext += ` _(${reqsym}: ${this.RequirementText})_`;
-        }
-        return fulltext.trim();
-    }
-
-    get TotalCost() {
-        if (this.pointCost >= 0) {
-            return this.pointCost;
-        }
-        let effectCost = (this.EffectCost || 0) * ABILITY_POINTCOST_FACTOR;
-        let reqCost = this.applyRequirementDiscount(effectCost) || effectCost;
-        let costCost = this.applyCostDiscount(reqCost) || reqCost;
-        return costCost;
-    }
-
-    get EffectCost() {
-        if (this.effectName == "custom") {
-            return Math.max(0, this.effectCost ?? 10);
-        }
-        return Math.max(0, this.abilityEffect
-            ?.getCost(this.effectX)
-            ?? 10
-        );
-    }
-
-    applyRequirementDiscount(cost) {
-        return this.cleanse(this.abilityRequirement
-            ?.getCost(this.requirementX, cost)
-            ?? cost
-        );
-    }
-
-    applyCostDiscount(cost) {
-        return this.cleanse(this.abilityCost
-            ?.getCost(this.costX, cost)
-            ?? cost
-        );
-    }
-
-    cleanse(cost) {
-        return Math.ceil(Math.max(0, cost));
     }
 }
 export default Ability;
--- CardGenerator/react-canvas-draw/src/Data/Creature.js
+++ CardGenerator/react-canvas-draw/src/Data/Creature.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { arraySum, getDateString, isImage } from "../Utility/Utility";
+import { getDateString, isImage } from "../Utility/Utility";
 import { VERSION } from "../Version";
 import Ability, { inflateAbility } from "./Ability/Ability";
 import BiomeModifier, { biomeList } from "./BiomeModifier";
@@ -113,45 +113,6 @@ class Creature {
         return this.basePower + this.getBiomeModifier(biome);
     }
 
-    getTotalCost() {
-        console.error("DONT CALL THIS METHOD (call costSpec.getTotalCost() instead)");
-        let cost = 0;
-        //Base Power
-        cost += Math.max(0, Math.ceil(this.basePower)) * 2;
-        //Biome Modifiers
-        cost += Math.ceil(arraySum(
-            Object.keys(this.biomeModifiers)
-                .map(bm => this.biomeModifiers[bm].modifier)
-        )
-            / 2);
-        //Abilities
-        cost += Math.ceil(this.abilityCost || 0);//test
-        cost += Math.max(0, arraySum(this.abilities, a => (a.TotalCost || 0)));
-        //Rest
-        if (this.rest > 0) {
-            const restSqr = Math.pow(this.rest, 2);
-            cost += -(Math.min(restSqr, this.rest * 5) + Math.max(1, restSqr * 0.1));
-            // cost *= 0.75 + 0.25 * (1 / ((this.rest-1) || 1));
-        }
-        //
-        cost = Math.round(cost);
-        cost = Math.max(cost, this.basePower);
-        //
-        return cost;
-    }
-
-    getFinalCost() {
-        console.error("DONT CALL THIS METHOD (call costSpec.getTotalCost() instead)");
-        let total = this.getTotalCost();
-        let discount = total / 10;
-        return Math.ceil(total - discount);
-    }
-
-    getStarCount() {
-        console.error("DONT CALL THIS METHOD (call costSpec.getStarCount() instead)");
-        let total = this.getTotalCost();
-        return Math.ceil(Math.sqrt(total * 0.75));
-    }
 }
 
 export default Creature;
